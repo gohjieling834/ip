76967d7b027b7b14f1f455ed69187ff1bbe43451
jieling <jieling834@gmail.com>
2025-08-22 17:43:08 +0800

    Add Greet/Exit lines and rename chatbot
    - Added greeting and exit lines
    - Renamed chatbot from Duke to Berry
    - Renamed file Duke.java to Berry.java

--- src/main/java/Berry.java
+++ src/main/java/Berry.java
@@ -1,10 +1,15 @@
-public class Duke {
+public class Berry {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        String chatbotName = "Berry";
+        System.out.println("Hello! I'm " + chatbotName + "\nWhat can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+
+
     }
 }
