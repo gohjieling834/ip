4e00954d851bd6d4b72f60f5c3a1c15b0feb3f6e
jieling <jieling834@gmail.com>
2025-08-29 03:53:02 +0800

    Add toggleTaskStatus method
    Added the toggleTaskStatus method so that the user can mark/unmark the task.
    
    Updated the addTask and list method to work with Task objects instead of Strings.

--- src/main/java/Berry.java
+++ src/main/java/Berry.java
@@ -4,7 +4,7 @@ public class Berry {
     public static void main(String[] args) {
         String chatbotName = "Berry";
         String userCommand;
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];   //array of Task objects
         int numberOftasks = 0;
         Scanner in = new Scanner(System.in);
         System.out.println("‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧\nHello! I'm " + chatbotName + "\nWhat can I do for you?\n‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧\n");
@@ -12,6 +12,8 @@ public class Berry {
         while (!userCommand.equals("bye")) {
             if (userCommand.equals("list")) {
                 list(tasks, numberOftasks);
+            } else if (userCommand.contains("mark")) {
+                toggleTaskStatus(userCommand, tasks);
             } else {
                 addTask(userCommand, tasks, numberOftasks);
                 numberOftasks++;
@@ -21,16 +23,33 @@ public class Berry {
         System.out.println("\n‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧\nBye. Hope to see you again soon!\n‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧");
     }
 
-    public static void addTask(String command, String[] tasks, int numberOftasks) {
-        tasks[numberOftasks] = command;
+    public static void addTask(String command, Task[] tasks, int numberOftasks) {
+        tasks[numberOftasks] = new Task(command);
         System.out.println("\n‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧\nadded: " + command + "\n‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧\n");
     }
 
-    public static void list(String[] tasks, int numberOftasks) {
+    public static void list(Task[] tasks, int numberOftasks) {
         System.out.println("\n‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧");
         for (int i = 0; i < numberOftasks; i++) {
-            System.out.println(i + 1 + ". " + tasks[i]);
+            System.out.println(i + 1 + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
         }
         System.out.println("‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧\n");
+    }
+
+    public static void toggleTaskStatus(String command, Task[] tasks) {
+        int dividerPosition = command.indexOf(" ");
+
+        //task number to identify which task to mark/unmark
+        int taskNumber = Integer.parseInt(command.substring(dividerPosition).trim()) - 1;
+        System.out.println("\n‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧");
+        if (command.contains("un")) {
+            tasks[taskNumber].unmarkAsDone();
+            System.out.println("Okay, I've marked this task as not done yet:\n[ ] "
+                    + tasks[taskNumber].getDescription() + "\n‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧\n");
+        } else {
+            tasks[taskNumber].markAsDone();
+            System.out.println("Nice! I've marked this task as done:\n[X] "
+                    + tasks[taskNumber].getDescription() + "\n‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧\n");
+        }
     }
 }
