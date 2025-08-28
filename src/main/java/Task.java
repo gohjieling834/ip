0d1544f1982bc5012b5c32560402c7c0cb862dfd
jieling <jieling834@gmail.com>
2025-08-29 03:08:44 +0800

    Add Task class
    Added a Task class to represent tasks and encapsulate their related data.

---
+++ src/main/java/Task.java
@@ -0,0 +1,37 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description){
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public String getStatusIcon() {
+        return (isDone? "X" : " ");
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void unmarkAsDone() {
+        
+    }
+}
