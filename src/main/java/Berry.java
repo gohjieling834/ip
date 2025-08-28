3c2962c5a210f0005a45a6341a48d58102e07aa3
jieling <jieling834@gmail.com>
2025-08-29 02:10:07 +0800

    Update echo method
    Moved the variables and while loop in the echo method into main.

--- src/main/java/Berry.java
+++ src/main/java/Berry.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class Berry {
     public static void main(String[] args) {
         String chatbotName = "Berry";
+        String userCommand;
+        Scanner in = new Scanner(System.in);
         System.out.println("‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧\nHello! I'm " + chatbotName + "\nWhat can I do for you?\n‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧\n");
-        echo();
+        userCommand = in.nextLine();  //get command from user
+        while (!userCommand.equals("bye")) {
+            echo(userCommand);
+            userCommand = in.nextLine();
+        }
         System.out.println("\n‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧\nBye. Hope to see you again soon!\n‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧");
     }
 
-    public static void echo(){
-        String inputText;
-        Scanner in = new Scanner(System.in);
-        inputText = in.nextLine();
-        while (!inputText.equals("bye")) {
-            System.out.println("\n‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧\n" + inputText + "\n‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧\n");
-            inputText = in.nextLine();
-        }
+    public static void echo(String command) {
+        System.out.println("\n‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧\n" + command + "\n‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧\n");
         return;
     }
 }
