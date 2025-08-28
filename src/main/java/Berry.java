557965eea05ad79d498d089fb01f440a05044c97
jieling <jieling834@gmail.com>
2025-08-29 01:52:21 +0800

    Add method 'echo'
    The method 'echo' echos commands entered by the user, and exists when the user types the command 'bye'.

--- src/main/java/Berry.java
+++ src/main/java/Berry.java
@@ -1,15 +1,21 @@
+import java.util.Scanner;
+
 public class Berry {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
         String chatbotName = "Berry";
-        System.out.println("Hello! I'm " + chatbotName + "\nWhat can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧\nHello! I'm " + chatbotName + "\nWhat can I do for you?\n‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧\n");
+        echo();
+        System.out.println("\n‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧\nBye. Hope to see you again soon!\n‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧");
+    }
 
-
+    public static void echo(){
+        String inputText;
+        Scanner in = new Scanner(System.in);
+        inputText = in.nextLine();
+        while (!inputText.equals("bye")) {
+            System.out.println("\n‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧\n" + inputText + "\n‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧\n");
+            inputText = in.nextLine();
+        }
+        return;
     }
 }
