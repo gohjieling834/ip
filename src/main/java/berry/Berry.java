91f272a8a246a1327f3da496024b44c40e120546
jieling <jieling834@gmail.com>
2025-09-15 18:06:55 +0800

    Update code to use ArrayList for tasks

--- src/main/java/berry/Berry.java
+++ src/main/java/berry/Berry.java
@@ -6,21 +6,21 @@ import berry.task.Task;
 import berry.task.Todo;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Berry {
     public static final String DIVIDER = "‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧";
     public static final String CHATBOT_NAME = "Berry";
-    public static final int TASK_CAPACITY = 100;
     public static final Scanner IN = new Scanner(System.in);
 
     public static void main(String[] args) {
         String userInput;
-        Task[] tasks = new Task[TASK_CAPACITY];   //array of Task objects
+        ArrayList<Task> tasks = new ArrayList<>();
         int taskCount = 0;
 
         printHelloMessage();
         while (true) {
-            userInput = getUserInput(IN);   //get command from user
+            userInput = getUserInput(IN);
             taskCount = executeCommand(userInput, tasks, taskCount);
         }
     }
@@ -39,9 +39,9 @@ public class Berry {
         System.out.println("\n" + DIVIDER + "\n" + errorMessage + "\n" + DIVIDER + "\n");
     }
 
-    public static void printAddTaskMessage(Task task, int taskCount) {
-        System.out.println("\n" + DIVIDER + "\nGot it. I've added this task:\n  " + task
-                + "\nNow you have " + (taskCount + 1) + " tasks in the list.\n" + DIVIDER + "\n");
+    public static void printAddTaskMessage(ArrayList<Task> tasks) {
+        System.out.println("\n" + DIVIDER + "\nGot it. I've added this task:\n  " + tasks.get(tasks.size()-1)
+                + "\nNow you have " + tasks.size() + " tasks in the list.\n" + DIVIDER + "\n");
     }
 
     public static String getUserInput(Scanner in) {
@@ -74,7 +74,7 @@ public class Berry {
         return task.split("/");
     }
 
-    public static int executeCommand(String userInput, Task[] tasks, int taskCount) {
+    public static int executeCommand(String userInput, ArrayList<Task> tasks, int taskCount) {
         try {
             String userCommand = extractCommand(userInput);
             switch (userCommand) {
@@ -109,16 +109,16 @@ public class Berry {
         return taskCount;
     }
 
-    public static void addTodo(String userInput, Task[] tasks, int taskCount) {
+    public static void addTodo(String userInput, ArrayList<Task> tasks, int taskCount) {
         if (userInput.trim().length() < 5) {
             throw new BerryException("Your description of todo cannot be empty!");
         }
         String description = userInput.substring(5).trim();
-        tasks[taskCount] = new Todo(description);
-        printAddTaskMessage(tasks[taskCount], taskCount);
+        tasks.add(new Todo(description));
+        printAddTaskMessage(tasks);
     }
 
-    public static void addDeadline(String userInput, Task[] tasks, int taskCount) {
+    public static void addDeadline(String userInput, ArrayList<Task> tasks, int taskCount) {
         String[] taskDetails = extractDetails(userInput);
         if (taskDetails.length < 2) {
             throw new ArrayIndexOutOfBoundsException("Please enter both task description and by when. Thank you :)");
@@ -126,11 +126,11 @@ public class Berry {
         int startIndexOfBy = taskDetails[1].indexOf("by") + 2;  // + 2 because the substring start index should begin after by
         String description = taskDetails[0].trim();
         String by = taskDetails[1].substring(startIndexOfBy).trim();
-        tasks[taskCount] = new Deadline(description, by);
-        printAddTaskMessage(tasks[taskCount], taskCount);
+        tasks.add(new Deadline(description, by));
+        printAddTaskMessage(tasks);
     }
 
-    public static void addEvent(String userInput, Task[] tasks, int taskCount) {
+    public static void addEvent(String userInput, ArrayList<Task> tasks, int taskCount) {
         String[] taskDetails = extractDetails(userInput);
         if (taskDetails.length < 3) {
             throw new ArrayIndexOutOfBoundsException("Please enter all the event detail (description, from, to). Thank you :)");
@@ -140,35 +140,34 @@ public class Berry {
         String description = taskDetails[0].trim();
         String from = taskDetails[1].substring(startIndexOfFrom).trim();
         String to = taskDetails[2].substring(startIndexOfTo).trim();
-        tasks[taskCount] = new Event(description, from, to);
-        printAddTaskMessage(tasks[taskCount], taskCount);
+        tasks.add(new Event(description, from, to));
+        printAddTaskMessage(tasks);
     }
 
-    public static void printList(Task[] tasks, int taskCount) {
+    public static void printList(ArrayList<Task> tasks, int taskCount) {
         if (taskCount == 0) {
             throw new BerryException("There's no tasks in the list. Would you like to start adding tasks?");
         }
         System.out.println("\n" + DIVIDER);
         for (int i = 0; i < taskCount; i++) {
-            System.out.println(i + 1 + "." + tasks[i]);
+            System.out.println(i + 1 + "." + tasks.get(i));
         }
         System.out.println(DIVIDER + "\n");
     }
 
-    public static void toggleTaskStatus(String userInput, Task[] tasks) {
+    public static void toggleTaskStatus(String userInput, ArrayList<Task> tasks) {
         int dividerPosition = userInput.indexOf(" ");
 
-        //task number to identify which task to mark/unmark
         try {
             int taskNumber = Integer.parseInt(userInput.substring(dividerPosition).trim()) - 1;
             if (userInput.contains("un")) {
-                tasks[taskNumber].markAsUndone();
+                tasks.get(taskNumber).markAsUndone();
                 System.out.println("\n" + DIVIDER + "\n" + "Okay, I've marked this task as not done yet:\n  "
-                        + tasks[taskNumber] + "\n" + DIVIDER + "\n");
+                        + tasks.get(taskNumber) + "\n" + DIVIDER + "\n");
             } else {
-                tasks[taskNumber].markAsDone();
+                tasks.get(taskNumber).markAsDone();
                 System.out.println("\n" + DIVIDER + "\n" + "Nice! I've marked this task as done:\n  "
-                        + tasks[taskNumber] + "\n" + DIVIDER + "\n");
+                        + tasks.get(taskNumber) + "\n" + DIVIDER + "\n");
             }
         } catch (NumberFormatException e) {
             printErrorMessage("Sorry, I don't know which task to delete. Please enter the task number, thank you! :)");
