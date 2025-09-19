4d7c28ca8f7f02f5554c6f9dbbe6855e594727ee
jieling <jieling834@gmail.com>
2025-09-20 00:21:54 +0800

    Improve code readability
    Extracted loadData and printHelloMessage into
    start method.

--- src/main/java/berry/Berry.java
+++ src/main/java/berry/Berry.java
@@ -19,39 +19,50 @@ public class Berry {
     public static final String FILEPATH = "./data/berry.txt";
     public static final String TEMPFILEPATH = "./data/temp.txt";
     public static final Scanner IN = new Scanner(System.in);
+    public static final File DATAFILE = new File(FILEPATH);
 
     public static void main(String[] args) {
         String userInput;
         ArrayList<Task> tasks = new ArrayList<>();
-        checkDirectoryExists();
-        try {
-            loadData(tasks);
-        } catch (FileNotFoundException e) {
-            System.out.println("File doesn't exist");
-        }
-        printHelloMessage();
+
+        start(tasks);
         while (true) {
             userInput = getUserInput(IN);
             executeCommand(userInput, tasks);
         }
     }
 
+    private static void start(ArrayList<Task> tasks) {
+        try {
+            checkDirectoryExists();
+            loadData(tasks);
+            printHelloMessage();
+        } catch (FileNotFoundException | BerryException e) {
+            printErrorMessage(e.getMessage());
+        }
+    }
+
     private static void loadData(ArrayList<Task> tasks) throws FileNotFoundException {
-        File dataFile = new File(FILEPATH);
-        Scanner scan = new Scanner(dataFile);
+        Scanner scan = new Scanner(DATAFILE);
         String currentLine;
         String[] taskDetails;
-        while (scan.hasNext()){
+        while (scan.hasNext()) {
             currentLine = scan.nextLine();
             taskDetails = currentLine.split("\\|");
-            if(taskDetails.length == 3){
-                tasks.add(new Todo (taskDetails[2].trim()));
-            } else if (taskDetails.length == 4) {
+            switch (taskDetails.length) {
+            case 3:
+                tasks.add(new Todo(taskDetails[2].trim()));
+                break;
+            case 4:
                 tasks.add(new Deadline(taskDetails[2].trim(), taskDetails[3].trim()));
-            } else {
+                break;
+            case 5:
                 tasks.add(new Event(taskDetails[2].trim(), taskDetails[3].trim(), taskDetails[4].trim()));
+                break;
+            default:
+                throw new BerryException("wrong data loaded.");
             }
-            tasks.get(tasks.size()-1).setDone(taskDetails[1].trim().equals("X"));
+            tasks.get(tasks.size() - 1).setDone(taskDetails[1].trim().equals("X"));
         }
         scan.close();
     }
@@ -62,31 +73,29 @@ public class Berry {
             directory.mkdir();
         }
         try {
-            checkFileExists(FILEPATH);
+            checkFileExists(DATAFILE);
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            printErrorMessage(e.getMessage());
         }
     }
 
-    private static void checkFileExists(String filePath) throws IOException {
-        File file = new File(filePath);
+    private static void checkFileExists(File file) throws IOException {
         if (!file.exists()) {
             file.createNewFile();
         }
     }
 
     private static void updateFile(int taskIndex, String userCommand, ArrayList<Task> tasks) throws IOException {
-        checkFileExists(TEMPFILEPATH);
-        File file = new File(FILEPATH);
         File tempFile = new File(TEMPFILEPATH);
+        checkFileExists(tempFile);
         FileWriter tempFileWriter = new FileWriter(tempFile);
-        Scanner scan = new Scanner(file);
+        Scanner scan = new Scanner(DATAFILE);
         String currentLine;
         int loopIndex = 0;
         while (scan.hasNext()) {
             currentLine = scan.nextLine();
-            if(loopIndex == taskIndex){
-                if(userCommand.equals("mark")){
+            if (loopIndex == taskIndex) {
+                if (userCommand.equals("mark")) {
                     tempFileWriter.write(tasks.get(loopIndex).fileFormat() + System.lineSeparator());
                 }
                 loopIndex++;
@@ -97,13 +106,13 @@ public class Berry {
         }
         scan.close();
         tempFileWriter.close();
-        file.delete();
-        tempFile.renameTo(file);
+        DATAFILE.delete();
+        tempFile.renameTo(DATAFILE);
     }
 
     private static void appendToFile(ArrayList<Task> tasks) throws IOException {
         FileWriter fw = new FileWriter(FILEPATH, true);
-        Task newestTask = tasks.get(tasks.size()-1);
+        Task newestTask = tasks.get(tasks.size() - 1);
         fw.write(newestTask.fileFormat() + System.lineSeparator());
         fw.close();
     }
@@ -186,14 +195,12 @@ public class Berry {
                 System.exit(0);
                 // Fallthrough
             }
-        } catch (BerryException e) {
-            System.out.println("\n" + DIVIDER + "\n" + e.getMessage() + "\n" + DIVIDER + "\n");
-        } catch (ArrayIndexOutOfBoundsException | IOException e) {
+        } catch (BerryException | ArrayIndexOutOfBoundsException | IOException e) {
             printErrorMessage(e.getMessage());
         }
     }
 
-    public static void addTodo(String userInput, ArrayList<Task> tasks) throws IOException{
+    public static void addTodo(String userInput, ArrayList<Task> tasks) throws IOException {
         if (userInput.trim().length() < 5) {
             throw new BerryException("Your description of todo cannot be empty!");
         }
@@ -262,7 +269,7 @@ public class Berry {
         } catch (IndexOutOfBoundsException e) {
             printErrorMessage("This task number does not exist! :|");
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            printErrorMessage(e.getMessage());
         }
     }
 
@@ -280,7 +287,7 @@ public class Berry {
         } catch (IndexOutOfBoundsException e) {
             printErrorMessage("This task number does not exist! :|");
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            printErrorMessage(e.getMessage());
         }
     }
 }
