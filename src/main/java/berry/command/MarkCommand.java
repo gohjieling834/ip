12afe967c0b61b3189a62747706b2901665f3f87
jieling <jieling834@gmail.com>
2025-10-03 01:27:58 +0800

    Refactor MarkCommand.java
    - Renamed variable `userInput` to `number` for better clarity, as it
      directly represents the task number to be marked/unmarked.
    - Replaced `userCommand` with a constant to avoid using a magic value
      directly in the code.
    - Added a variable `isMarkCommand` to differentiate between mark
      and unmark command.
    - Extracted the code in the if else cases into 2 methods, `markTask` and
      `unmarkTask`, reducing the if else cases.

--- src/main/java/berry/command/MarkCommand.java
+++ src/main/java/berry/command/MarkCommand.java
@@ -2,6 +2,7 @@ package berry.command;
 
 import berry.data.TaskList;
 import berry.storage.Storage;
+import berry.task.Task;
 import berry.ui.Ui;
 
 import java.io.IOException;
@@ -10,12 +11,13 @@ import java.io.IOException;
  * Represents a command that marks or unmarks a task in the list.
  */
 public class MarkCommand extends Command {
-    private final String userCommand;
-    private final String userInput;
+    private static final String COMMAND = "mark";
+    private final String number;
+    private final boolean isMarkCommand;
 
-    public MarkCommand(String userCommand, String userInput) {
-        this.userCommand = userCommand;
-        this.userInput = userInput;
+    public MarkCommand(String number, boolean isMarkCommand) {
+        this.number = number;
+        this.isMarkCommand = isMarkCommand;
     }
 
     /**
@@ -32,30 +34,52 @@ public class MarkCommand extends Command {
      * @param storage Storage instance used to update berry.txt.
      */
     public void execute(TaskList tasks, Ui ui, Storage storage) {
-        int dividerPosition = userInput.indexOf(" ");
 
         try {
-            int taskNumber = Integer.parseInt(userInput.substring(dividerPosition).trim()) - 1;
-            boolean isUnmarkCommand = userInput.contains("un");
-            boolean isDone = tasks.getTask(taskNumber).isDone();
-            if (isUnmarkCommand && isDone) {
-                tasks.getTask(taskNumber).markAsUndone();
-                ui.printMarkTaskMessage(tasks.getTask(taskNumber), "Okay, I've marked this task as not done yet:\n  ");
-            } else if (isUnmarkCommand && !isDone) {
-                ui.printMarkTaskMessage(tasks.getTask(taskNumber), "This task is already marked as not done:\n  ");
-            } else if (!isUnmarkCommand && isDone) {
-                ui.printMarkTaskMessage(tasks.getTask(taskNumber), "This task is already marked as done:\n  ");
+            int taskNumber = Integer.parseInt(number.trim()) - 1;
+            Task task = tasks.getTask(taskNumber);
+            if (!isMarkCommand) {
+                unmarkTask(task, ui);
             } else {
-                tasks.getTask(taskNumber).markAsDone();
-                ui.printMarkTaskMessage(tasks.getTask(taskNumber), "Nice! I've marked this task as done:\n  ");
+                markTask(task, ui);
             }
-            storage.updateFile(taskNumber, userCommand, tasks.getList());
+            storage.updateFile(taskNumber, COMMAND, tasks.getList());
         } catch (NumberFormatException e) {
             ui.printErrorMessage("Sorry, I don't know which task to mark/unmark. Please enter the task number, thank you! :)");
         } catch (IndexOutOfBoundsException e) {
             ui.printErrorMessage("This task number does not exist! :|");
         } catch (IOException e) {
             ui.printErrorMessage(e.getMessage());
+        }
+    }
+
+    /**
+     * Marks a task as done if it is not already done.
+     *
+     * @param task The task to be marked as done.
+     * @param ui   The UI handler to print messages to the user.
+     */
+    private void markTask(Task task, Ui ui) {
+        if (task.isDone()) {
+            ui.printMarkTaskMessage(task, "This task is already marked as done:\n  ");
+        } else {
+            task.markAsDone();
+            ui.printMarkTaskMessage(task, "Nice! I've marked this task as done:\n  ");
+        }
+    }
+
+    /**
+     * Marks a task as not done if it is currently done.
+     *
+     * @param task The task to be marked as not done.
+     * @param ui   The UI handler to print messages to the user.
+     */
+    private void unmarkTask(Task task, Ui ui) {
+        if (!task.isDone()) {
+            ui.printMarkTaskMessage(task, "This task is already marked as not done:\n  ");
+        } else {
+            task.markAsUndone();
+            ui.printMarkTaskMessage(task, "Okay, I've marked this task as not done yet:\n  ");
         }
     }
 }
