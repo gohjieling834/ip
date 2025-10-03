c0c8d70e8fc33f18d8deefd4d09792025f1c92b2
jieling <jieling834@gmail.com>
2025-10-03 15:10:51 +0800

    Update AddEventCommand.java
    - Added 2 new methods to check if details are valid and handle
      invalid details.

--- src/main/java/berry/command/AddEventCommand.java
+++ src/main/java/berry/command/AddEventCommand.java
@@ -35,14 +35,45 @@ public class AddEventCommand extends Command {
      */
     public void execute(TaskList tasks, Ui ui, Storage storage) throws IOException {
         String[] taskDetails = Parser.splitDetails(taskDetailsInput);
-        if (taskDetails.length < 3) {
-            throw new ArrayIndexOutOfBoundsException("Please enter all the event detail (description, from, to). Thank you :)");
-        }
+        checkDetailsFormat(taskDetails);
+
         int startIndexOfFrom = taskDetails[1].indexOf("from") + 4;  // + 4 because the substring start index should begin after from
         int startIndexOfTo = taskDetails[2].indexOf("to") + 2;  // + 2 because the substring start index should begin after to
         String description = taskDetails[0].trim();
         String from = taskDetails[1].substring(startIndexOfFrom).trim();
         String to = taskDetails[2].substring(startIndexOfTo).trim();
+
+        checkDetails(description, from, to);
+        tasks.addTask(new Event(description, from, to));
+        storage.appendToFile(tasks.getList());
+        ui.printAddTaskMessage(tasks.getList());
+    }
+
+    /**
+     * Checks if the task details are entered in the correct format.
+     *
+     * @param details The task details.
+     * @throws ArrayIndexOutOfBoundsException If there is any missing details (e.g. description, from, to).
+     * @throws BerryException                 If the wrong format is used.
+     */
+    private void checkDetailsFormat(String[] details) {
+        if (details.length < 3) {
+            throw new ArrayIndexOutOfBoundsException("Please enter all the event detail (description, from, to). Thank you :)");
+        }
+        if (!details[1].contains("from") || !details[2].contains("to")) {
+            throw new BerryException("Wrong format. Please use the correct format!");
+        }
+    }
+
+    /**
+     * Checks if the task details are empty.
+     *
+     * @param description The task description.
+     * @param from        The task start date.
+     * @param to          The task end date.
+     * @throws BerryException If any of the details are empty.
+     */
+    private void checkDetails(String description, String from, String to) {
         if (description.isEmpty()) {
             throw new BerryException("Please enter event description! Thank you :)");
         }
@@ -52,8 +83,5 @@ public class AddEventCommand extends Command {
         if (to.isEmpty()) {
             throw new BerryException("Please enter when the event ends! Thank you :)");
         }
-        tasks.addTask(new Event(description, from, to));
-        storage.appendToFile(tasks.getList());
-        ui.printAddTaskMessage(tasks.getList());
     }
 }
